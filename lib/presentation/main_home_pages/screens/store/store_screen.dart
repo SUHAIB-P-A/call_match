89a2e5ed4903a89e688a7bc45b1b4ea9d43f59b4
Varun-K34 <appuvarun88@gmail.com>
2024-07-02 12:33:31 +0530

    Store_screen updation

--- lib/presentation/main_home_pages/screens/store/store_screen.dart
+++ lib/presentation/main_home_pages/screens/store/store_screen.dart
@@ -5,26 +5,8 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF8F8F8),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFF8F8F8),
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search, color: Color(0xFFC7445C)),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.menu, color: Color(0xFFC7445C)),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Store Screen Content Here'),
-      ),
+    return const Scaffold(
+      backgroundColor: Color(0xFFF8F8F8),
     );
   }
 }
