9e5354b44291f425f8a6216981e61298b6db651f
thegreatcode1 <suhaibpa72@gmail.com>
2024-07-02 15:04:49 +0530

    updated store

--- lib/presentation/main_home_pages/screens/store/store_screen.dart
+++ lib/presentation/main_home_pages/screens/store/store_screen.dart
@@ -1,3 +1,4 @@
+import 'package:call_match/presentation/main_home_pages/screens/home/widgets/add_coin_and_display.dart';
 import 'package:flutter/material.dart';
 
 class StoreScreen extends StatelessWidget {
@@ -5,8 +6,102 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFF8F8F8),
+    return Scaffold(
+      backgroundColor: const Color(0xffffdce0),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.red,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 40,
+                width: 40,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/profile.png"),
+                  ),
+                ),
+              ),
+              const AddCoinDisplayUI()
+            ],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Buy Coins",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text("Wallet"),
+                  Text("─────────"),
+                ],
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Column(
+                children: [
+                  Text("Earnings"),
+                  Text("─────────"),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(""),
+                  ),
+                ),
+              ),
+              const Text("100 Coins"),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
