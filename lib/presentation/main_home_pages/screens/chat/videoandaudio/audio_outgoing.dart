2837e7725f00c58780e7a1dad2ef5575150fe9ff
Varun-K34 <appuvarun88@gmail.com>
2024-07-19 13:21:00 +0530

    deletion

--- lib/presentation/main_home_pages/screens/chat/videoandaudio/audio_outgoing.dart
+++ lib/presentation/main_home_pages/screens/chat/videoandaudio/audio_outgoing.dart
@@ -1,6 +1,6 @@
 import 'package:call_match/presentation/main_home_pages/screens/chat/videoandaudio/widgets/after_call_accept_ui.dart';
 import 'package:call_match/presentation/main_home_pages/screens/chat/videoandaudio/widgets/imageandname.dart';
-import 'package:call_match/presentation/main_home_pages/screens/chat/videoandaudio/widgets/top_buttons.dart';
+//import 'package:call_match/presentation/main_home_pages/screens/chat/videoandaudio/widgets/top_buttons.dart';
 import 'package:flutter/material.dart';
 
 class AudioOutgoingUI extends StatelessWidget {
--- lib/presentation/main_home_pages/screens/store/payment_gateway/payment.dart
+++ lib/presentation/main_home_pages/screens/store/payment_gateway/payment.dart
@@ -1,6 +1,4 @@
 import 'package:call_match/presentation/main_home_pages/screens/store/payment_gateway/widgets/gateway_list.dart';
-import 'package:call_match/presentation/main_home_pages/screens/store/widget/scaech_menu.dart';
-import 'package:call_match/presentation/main_home_pages/widgets/profile_add_coin.dart';
 import 'package:flutter/material.dart';
 
 class PaymentScreenUI extends StatelessWidget {
